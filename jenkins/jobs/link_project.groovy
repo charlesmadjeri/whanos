b5f07770ff507c96bbdd49a28dbca7e9291bf7c3
Charles Madjeri <80175305+charlesmadjeri@users.noreply.github.com>
2024-12-08 21:19:36 +0100

    feat(jobs): add limk project job, still need testing
    Signed-off-by: Charles Madjeri <charles@madjeri.com>

--- jenkins/jobs/link_project.groovy
+++ jenkins/jobs/link_project.groovy
@@ -1,11 +1,79 @@
 freeStyleJob('link-project') {
-    disabled(false)
     parameters {
         stringParam('GIT_URL', '', 'Git repository URL')
         stringParam('PROJECT_NAME', '', 'Name of the project')
-        choiceParam('LANGUAGE', ['nodejs', 'python', 'java', 'befunge', 'c'], 'Project language')
     }
-    // steps {
-    //     shell("echo 'TODO: Link project ${PROJECT_NAME} (${LANGUAGE}) from ${GIT_URL}'")
-    // }
+
+    steps {
+        dsl {
+            text('''
+                folder('Projects')
+                
+                job("Projects/$PROJECT_NAME") {
+                    scm {
+                        git {
+                            remote {
+                                url('$GIT_URL')
+                            }
+                            branch('*/main')
+                        }
+                    }
+                    
+                    triggers {
+                        scm('* * * * *')  // Check every minute
+                    }
+                    
+                    steps {
+                        // Detect language and validate project
+                        shell("""
+                            WORKSPACE=\$PWD
+                            
+                            # Language detection
+                            if [ -f Makefile ]; then
+                                LANGUAGE="c"
+                            elif [ -f app/pom.xml ]; then
+                                LANGUAGE="java"
+                            elif [ -f package.json ]; then
+                                LANGUAGE="javascript"
+                            elif [ -f requirements.txt ]; then
+                                LANGUAGE="python"
+                            elif [ -f app/main.bf ]; then
+                                LANGUAGE="befunge"
+                            else
+                                echo "No valid Whanos project structure detected"
+                                exit 1
+                            fi
+                            
+                            # Check for multiple detection criteria
+                            COUNT=0
+                            [ -f Makefile ] && COUNT=$((COUNT+1))
+                            [ -f app/pom.xml ] && COUNT=$((COUNT+1))
+                            [ -f package.json ] && COUNT=$((COUNT+1))
+                            [ -f requirements.txt ] && COUNT=$((COUNT+1))
+                            [ -f app/main.bf ] && COUNT=$((COUNT+1))
+                            
+                            if [ $COUNT -gt 1 ]; then
+                                echo "Multiple language detection criteria found"
+                                exit 1
+                            fi
+                            
+                            # Build using appropriate image
+                            if [ -f Dockerfile ]; then
+                                # Use base image
+                                docker build -t ${PROJECT_NAME}:latest --build-arg BASE_IMAGE=whanos-$LANGUAGE-base:latest .
+                            else
+                                # Use standalone image
+                                docker build -t ${PROJECT_NAME}:latest -f /images/$LANGUAGE/Dockerfile .
+                            fi
+                            
+                            # Deploy if K8s manifest exists
+                            if [ -f manifest.yml ]; then
+                                kubectl apply -f manifest.yml
+                            fi
+                        """)
+                    }
+                }
+            '''.stripIndent())
+        }
+    }
 } 
