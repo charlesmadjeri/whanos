fd0420ae13614829ecdaf62d3846850f96e7a018
Charles Madjeri <80175305+charlesmadjeri@users.noreply.github.com>
2024-12-10 20:28:07 +0100

    fix(jobs): fix link-project job and begin git auth implementation
    Signed-off-by: Charles Madjeri <charles@madjeri.com>

--- jenkins/jobs/link_project.groovy
+++ jenkins/jobs/link_project.groovy
@@ -1,32 +1,38 @@
 freeStyleJob('link-project') {
     parameters {
-        stringParam('GIT_URL', '', 'Git repository URL')
+        stringParam('GIT_URL', '', 'Git repository URL (HTTPS or SSH)')
         stringParam('PROJECT_NAME', '', 'Name of the project')
+        stringParam('GIT_BRANCH', 'main', 'Git branch to build (default: main)')
+        credentialsParam('GIT_CREDENTIALS') {
+            type('com.cloudbees.plugins.credentials.common.StandardCredentials')
+            required(false)
+            description('Credentials to use for private repository (username/password or SSH key)')
+        }
     }
 
     steps {
         dsl {
             text('''
-                folder('Projects')
-                
                 job("Projects/$PROJECT_NAME") {
                     scm {
                         git {
                             remote {
-                                url('$GIT_URL')
+                                url("${GIT_URL}")
+                                if (GIT_CREDENTIALS) {
+                                    credentials(GIT_CREDENTIALS)
+                                }
                             }
-                            branch('*/main')
+                            branch("*/${GIT_BRANCH}")
                         }
                     }
                     
                     triggers {
-                        scm('* * * * *')  // Check every minute
+                        scm('* * * * *')
                     }
                     
                     steps {
-                        // Detect language and validate project
-                        shell("""
-                            WORKSPACE=\$PWD
+                        shell("""#!/bin/bash
+                            WORKSPACE=\\\$PWD
                             
                             # Language detection
                             if [ -f Makefile ]; then
@@ -41,32 +47,29 @@ freeStyleJob('link-project') {
                                 LANGUAGE="befunge"
                             else
                                 echo "No valid Whanos project structure detected"
+                                echo 'Files found: '
+                                tree
                                 exit 1
                             fi
                             
-                            # Check for multiple detection criteria
                             COUNT=0
-                            [ -f Makefile ] && COUNT=$((COUNT+1))
-                            [ -f app/pom.xml ] && COUNT=$((COUNT+1))
-                            [ -f package.json ] && COUNT=$((COUNT+1))
-                            [ -f requirements.txt ] && COUNT=$((COUNT+1))
-                            [ -f app/main.bf ] && COUNT=$((COUNT+1))
+                            [ -f Makefile ] && COUNT=\\\$((COUNT+1))
+                            [ -f app/pom.xml ] && COUNT=\\\$((COUNT+1))
+                            [ -f package.json ] && COUNT=\\\$((COUNT+1))
+                            [ -f requirements.txt ] && COUNT=\\\$((COUNT+1))
+                            [ -f app/main.bf ] && COUNT=\\\$((COUNT+1))
                             
-                            if [ $COUNT -gt 1 ]; then
+                            if [ \\\$COUNT -gt 1 ]; then
                                 echo "Multiple language detection criteria found"
                                 exit 1
                             fi
                             
-                            # Build using appropriate image
                             if [ -f Dockerfile ]; then
-                                # Use base image
-                                docker build -t ${PROJECT_NAME}:latest --build-arg BASE_IMAGE=whanos-$LANGUAGE-base:latest .
+                                docker build -t \\\${PROJECT_NAME}:latest --build-arg BASE_IMAGE=whanos-\\\$LANGUAGE-base:latest .
                             else
-                                # Use standalone image
-                                docker build -t ${PROJECT_NAME}:latest -f /images/$LANGUAGE/Dockerfile .
+                                docker build -t \\\${PROJECT_NAME}:latest -f /images/\\\$LANGUAGE/Dockerfile .
                             fi
                             
-                            # Deploy if K8s manifest exists
                             if [ -f manifest.yml ]; then
                                 kubectl apply -f manifest.yml
                             fi
