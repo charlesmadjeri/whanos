f319ccec3dc775568915b3b86eb01da395d3e570
Cadavre-chan <80175305+charlesmadjeri@users.noreply.github.com>
2024-12-14 20:02:24 +0100

    feat(registry-setup): modified base images jobs so that it automatically pushes images onto registry

--- jenkins/jobs/base_images_jobs.groovy
+++ jenkins/jobs/base_images_jobs.groovy
@@ -4,6 +4,26 @@ languages.each { lang ->
     freeStyleJob("Whanos base images/whanos-${lang}") {
         steps {
             shell('/opt/whanos/jenkins/scripts/build_base_image.sh ' + lang)
+            shell("""
+                cd /images/${lang}
+                docker build -t whanos-${lang}-base:latest -f Dockerfile.base .
+                docker build -t whanos-${lang}:latest -f Dockerfile .
+                
+                # Tag images for DigitalOcean registry
+                docker tag whanos-${lang}-base:latest registry.digitalocean.com/whanos-container-registry/whanos-${lang}-base:latest
+                docker tag whanos-${lang}:latest registry.digitalocean.com/whanos-container-registry/whanos-${lang}:latest
+            """)
+            shell("""
+                # Authenticate with DigitalOcean registry and push images
+                echo "\$DOCKER_PASSWORD" | docker login -u "\$DOCKER_USERNAME" --password-stdin registry.digitalocean.com
+                docker push registry.digitalocean.com/whanos-container-registry/whanos-${lang}-base:latest
+                docker push registry.digitalocean.com/whanos-container-registry/whanos-${lang}:latest
+            """)
+        }
+        wrappers {
+            credentialsBinding {
+                usernamePassword('DOCKER_USERNAME', 'DOCKER_PASSWORD', 'digitalocean-credentials')
+            }
         }
     }
 }
